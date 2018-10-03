02d29922984fc8c088348f5927c955234358304d
Vahid Davoudi <vahid_davoudi_e@yahoo.com>
2018-10-03 11:22:24 +0800

    Sparksline library is added , reuesable sparksline component is added

---
+++ src/components/sparklines.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine
+} from "react-sparklines";
+import _ from "lodash";
+
+function average(data) {
+  return _.round(_.sum(data) / data.length);
+}
+
+const chart = props => {
+  return (
+    <div>
+      <Sparklines data={props.data} svgHeight={200} svgWidth={180}>
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>{average(props.data)}</div>
+    </div>
+  );
+};
+export default chart;
--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,23 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-class WeatherList extends Component {
-  render() {
-    console.log(this.props.weather, "thiusdsa props");
-    let weatherList = null;
-    if (this.props.weather) {
-      weatherList = this.props.weather.map(weather => {
-        console.log(weather, "wheedede");
-        return (
-          <tr key={weather.city.name}>
-            <th> {weather.city.name}</th>
-            <th> {weather.city.population}</th>
-            <th> {weather.city.name}</th>
-          </tr>
-        );
-      });
-    }
+import Chart from "../components/sparklines";
 
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    console.log(cityData, "city data");
+    const temp = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humid = cityData.list.map(weather => weather.main.humidity);
+
+    return (
+      <tr key={cityData.city.name}>
+        <td> {cityData.city.name}</td>
+
+        <td>
+          <Chart data={temp} color="red" />
+        </td>
+        <td>
+          <Chart data={pressure} color="gray" />
+        </td>
+        <td>
+          {" "}
+          <Chart data={humid} color="blue" />
+        </td>
+      </tr>
+    );
+  }
+
+  render() {
     return (
       <div>
         <table className="table table-hover">
@@ -26,10 +37,10 @@ class WeatherList extends Component {
               <th>City</th>
               <th>Tempreture</th>
               <th>Pressure</th>
-              <th>Time</th>
+              <th>Humidity</th>
             </tr>
           </thead>
-          <tbody>{weatherList}</tbody>
+          <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
         </table>
       </div>
     );
